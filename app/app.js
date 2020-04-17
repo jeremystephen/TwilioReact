b1bed5c519b161e9023cee2d01900d0931016896
Jeremy Stephen <jds@damoola.com>
2020-04-17 05:34:41 -0400

    Commit 3

--- app/app.js
+++ app/app.js
@@ -3,16 +3,18 @@ import AppBar from 'material-ui/AppBar';
 import { render } from "react-dom";
 import "./styles/styles.css";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import * as Colors from 'material-ui/styles/colors'
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 /*import injectTapEventPlugin from "react-tap-event-plugin";*/
 /*injectTapEventPlugin();*/
 
+
 let dom = document.getElementById("app");
 render(
-    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme.palette.canvasColor='#fffde7')}>
             <div>
-            <AppBar title="React Twilio Video" />
+            <AppBar color="primary" title="Celery Video" />
             </div>
     </MuiThemeProvider>
     ,
--- server.js
+++ server.js
@@ -24,23 +24,19 @@ if(process.env.NODE_ENV === "DEV") { // Configuration for development environmen
 
 app.get('/token', function(request, response) {
     var identity = faker.name.findName();
- 
-    // Create an access token which we will sign and return to the client,
-    // containing the grant we just created
+
     var token = new AccessToken(
         process.env.TWILIO_ACCOUNT_SID,
         process.env.TWILIO_API_KEY,
         process.env.TWILIO_API_SECRET
     );
- 
-    // Assign the generated identity to the token
+
     token.identity = identity;
  
     const grant = new VideoGrant();
-    // Grant token access to the Video API features
+
     token.addGrant(grant);
- 
-    // Serialize the token to a JWT string and include it in a JSON response
+    
     response.send({
         identity: identity,
         token: token.toJwt()
