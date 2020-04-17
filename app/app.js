5276301d3cf772d2298cd12933b2b8116c7ba6f6
Jeremy Stephen <jds@damoola.com>
2020-04-17 04:19:59 -0400

    First Commit

--- app/app.js
+++ app/app.js
@@ -5,8 +5,8 @@ import "./styles/styles.css";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import injectTapEventPlugin from "react-tap-event-plugin";
-injectTapEventPlugin();
+/*import injectTapEventPlugin from "react-tap-event-plugin";*/
+/*injectTapEventPlugin();*/
 
 let dom = document.getElementById("app");
 render(
